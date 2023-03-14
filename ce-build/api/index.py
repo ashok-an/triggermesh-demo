8d6a786c2f3082c4fbbdbeec01b5e5ec61f14633
Ashok Nagaraj <nashok@cisco.com>
2023-03-14 14:36:12 +0530

    Renaming to build

---
+++ ce-build/api/index.py
@@ -0,0 +1,53 @@
+import datetime
+import json
+import os
+import random
+import time
+import uuid
+
+from flask import Flask, make_response, request
+from faker import Faker
+from cloudevents.http import CloudEvent
+from cloudevents.conversion import to_structured
+
+
+start = datetime.datetime.now()
+_type = os.getenv('TASK_TYPE', '[BUILD]')
+fake = Faker()
+app = Flask(__name__)
+
+
+def run_task():
+  time.sleep(random.randrange(2, 5))
+  status = random.choice([True, True, True, True, False]) # 5:1
+  return status
+
+
+@app.route('/', methods=['GET'])
+@app.route('/healthz', methods=['GET'])
+def get_healthz():
+  return {"message": f"{_type}: up since {start}" }
+
+
+@app.route('/ce', methods=['GET', 'POST'])
+def get_ce():
+  status = run_task()
+  attributes = {
+    "id": str(uuid.uuid4()),
+    "type": "demo.tm.{}.{}".format(_type, 'pass' if status else 'fail') ,
+    "source": "producer.tm.demo.{}".format(_type),
+  }
+  if request.method == 'POST':
+    data = json.loads(request.data)
+    attributes['custom'] = data.get('input', 'no-input')
+
+  data = {'message': fake.text(), 'name': fake.name(), 'email': fake.email(), 'status': run_task() }
+
+  event = CloudEvent(attributes, data)
+  headers, body = to_structured(event)
+
+  response = make_response(body)
+  response.headers = headers
+
+  return response
+
